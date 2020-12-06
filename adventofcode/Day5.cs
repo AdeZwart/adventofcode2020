bb4a22c3769d4f696fbe69700413e4c0493384bc
André de Zwart <a.dezwart@evident.nl>
2020-12-06 13:11:55 +0100

    Adds day 5

--- adventofcode/Day5.cs
+++ adventofcode/Day5.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace adventofcode
 {
@@ -9,11 +9,108 @@ namespace adventofcode
         public static void PartOne(string[] fileLines)
         {
             Console.WriteLine("========== AdventOfCode Day 5 - Part One ==========");
+
+            var seatIDs = GetSortedPasses(fileLines);
+            var highestSeatId = seatIDs.Last();
+
+            Console.Write($"The highest seat ID is '{highestSeatId}'.\r\n");
         }
 
         public static void PartTwo(string[] fileLines)
         {
             Console.WriteLine("========== AdventOfCode Day 5 - Part Two ==========");
+
+            var seatIDs = GetSortedPasses(fileLines);
+
+            var mySeatId = 0;
+
+            for (int i = seatIDs.First(); i < seatIDs.Last(); i++)
+            {
+                if (!seatIDs.Contains(i))
+                {
+                    mySeatId = i;
+                    break;
+                }
+            }
+
+            Console.Write($"My unique seat ID is '{mySeatId}'.");
+        }
+
+        private static List<int> GetSortedPasses(string[] fileLines)
+        {
+            var seatIDs = new List<int>();
+
+            foreach (var line in fileLines)
+            {
+                var rowIndicators = line.Substring(0, 7);
+                var row = GetRowIndex(rowIndicators);
+
+                var columnIndicators = line.Substring(7);
+                var column = GetColumnIndex(columnIndicators);
+
+                var seatId = (row * 8) + column;
+                seatIDs.Add(seatId);
+            }
+
+            seatIDs.Sort();
+            return seatIDs;
+        }
+
+        private static int GetRowIndex(string rowIndicators)
+        {
+            var upperBoundary = 127;
+            var lowerBoundary = 0;
+
+            foreach (var i in rowIndicators)
+            {
+                var range = (upperBoundary - lowerBoundary);
+                if (i == 'B')
+                {
+                    // we're going backward
+                    lowerBoundary += (range / 2) + 1;
+                }
+
+                if (i == 'F')
+                {
+                    // we're going forward
+                    upperBoundary -= (range / 2) + 1;
+                }
+            }
+
+            if (upperBoundary != lowerBoundary)
+            {
+                Console.WriteLine($"ERROR ERROR ERROR: {nameof(lowerBoundary)}: {lowerBoundary} && {nameof(upperBoundary)}:{upperBoundary}");
+            }
+
+            return upperBoundary;
+        }
+
+        private static int GetColumnIndex(string columnIndicators)
+        {
+            var upperBoundary = 7;
+            var lowerBoundary = 0;
+
+            foreach (var i in columnIndicators)
+            {
+                var range = (upperBoundary - lowerBoundary);
+                if (i == 'R')
+                {
+                    // we're going backward
+                    lowerBoundary += (range / 2) + 1;
+                }
+
+                if (i == 'L')
+                {
+                    // we're going forward
+                    upperBoundary -= (range / 2) + 1;
+                }
+            }
+
+            if (upperBoundary != lowerBoundary)
+            {
+                Console.WriteLine($"ERROR ERROR ERROR: {nameof(lowerBoundary)}: {lowerBoundary} && {nameof(upperBoundary)}:{upperBoundary}");
+            }
+            return upperBoundary;
         }
     }
 }
